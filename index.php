2bc8f8989a850e80ce44df33611cd49156c76b3a
Konstantin Bondarenko <89511582818@ya.ru>
2016-08-06 17:11:55 +0300

    Add content

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My second project</title>
+</head>
+<body>
+	<div>
+		<p>Lorem ipsum dolor sit amet.</p>
+	</div>
+	<div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, eum?</p>
+	</div>
+</body>
+</html>
